692aed8a5fab6db59eb7521f3742afe5f77869d4
mariaclara287 <arboledamariaclara7@gmail.com>
2026-03-10 21:19:20 +0000

    ejercicio

---
+++ 10-03/ejercicio2.js
@@ -0,0 +1,18 @@
+let num1 = 16;
+console.log(num1);
+let num2 = 4;
+console.log(num2);
+let resultado = num1 / num2;
+console.log(resultado)
+
+let suma = num1 + num2;
+console.log(suma)
+
+let resta = num1 - num2;
+console.log(resta)
+
+let multiplicacion = num1 * num2;
+console.log(multiplicacion)
+
+let division = num1 / num2;
+console.log(division)

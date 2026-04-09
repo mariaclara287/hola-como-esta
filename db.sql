c1491c23d1cbc403acb1b2188125ec5c6b866d31
mariaclara287 <arboledamariaclara7@gmail.com>
2026-04-09 16:12:38 -0500

    Enhance db.sql with comments on table structures
    Added comments to table creation statements for clarity.

--- db.sql
+++ db.sql
@@ -3,18 +3,28 @@ Incluir el script del modelo físico de su PPI (por ahora crear las tablas)
 
 create database cryptoguard;
 use cryptoguard;
-create table producto (   id int primary key, bitcoin varchar(120));
+create table producto
+(id int primary key, // es entero porque se guarda un numero
+bitcoin varchar(120));es varcgar porque es por letra
 
-create table ventas( id int primary key, ProductosVendidos int,
-fechaDeVenta datetime, ValorDeLaVenta int, productoPorVender(120))
+create table ventas( id int primary key // es entero porque se guarda un numero
+ProductosVendidos int // es un numero porque es la cantidad de productos 
+fechaDeVenta datetime // es un numero por la fecha de ventas
+ValorDeLaVenta int, // es un numero que es el valor de las ventas
+productoPorVender(120))//es varchar porque es un numero
 
-create comprador(documento int primary key, PrimerNombre varchar(50),apellido varchar(50));
+create comprador(documento int primary key,//es entero porque se guarda un numero
+	PrimerNombre varchar(50),//es varchar porque es un numero
+	apellido varchar(50));es varchar porque es un numero
 
-create table comentarios (id int primary key, comprador varchar(50),
-vendedor varchar(50), administrador varchar(50));
+create table comentarios (id int primary key,//es entero porque se guarda un numero
+	comprador varchar(50)//es varchar porque es un numero
+vendedor varchar(50),//es varchar porque es un numero
+	administrador varchar(50));//es varchar porque es un numero
 
-create table vendedor(documento int primary key,
-nombre varchar(50),apellido varchar(50))
+create table vendedor(documento int primary key,//es entero porque se guarda un numero
+nombre varchar(50),//es varchar porque es un numero
+	apellido varchar(50))//es varchar porque es un numero
 
 
 
